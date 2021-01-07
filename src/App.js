dac9faf43ee80cc8c5871ac2bf3fb7946b5488da
juan <juancraft12yt@gmail.com>
2021-01-07 00:00:02 -0300

    Update App initial page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import Title from "./components/title/title.js";
+import HomePage from "./components/homePage/homePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PokemonCard from "./components/pokemonCard/pokemonCard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/pokemon/:name" exact>
+            <PokemonCard />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
