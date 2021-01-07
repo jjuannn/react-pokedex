aa824e977522efbb41d499f72bd1fc77f0c1eda9
juan <juancraft12yt@gmail.com>
2021-01-07 00:00:23 -0300

    Implement buttons for paginator (not implemented yet!)

---
+++ src/components/buttons/buttons.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./buttons.css";
+export default function Buttons({ text }) {
+  return (
+    <button className="btn btn-danger single-button">
+      {text}{" "}
+      {text === "Next page" ? (
+        <i className="fas fa-arrow-right"></i>
+      ) : (
+        <i className="fas fa-arrow-left"></i>
+      )}
+    </button>
+  );
+}
