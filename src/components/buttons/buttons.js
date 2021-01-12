0377a933fedf419cee76309d7a15c160265e7e9f
juan <juancraft12yt@gmail.com>
2021-01-11 22:10:35 -0300

    Implement pagination for pokemon list

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./buttons.css";
-export default function Buttons({ text }) {
+export default function Buttons({ text, onClick, disabled }) {
   return (
-    <button className="btn btn-danger single-button">
+    <button
+      className="btn btn-danger single-button"
+      onClick={onClick}
+      disabled={disabled}
+    >
       {text}{" "}
       {text === "Next page" ? (
         <i className="fas fa-arrow-right"></i>
--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,19 +1,44 @@
 import React, { useState } from "react";
 import useFetch from "../../hook/useFetch.js";
 import { getPage } from "../../api/api.js";
-import PokemonIndexList from "../pokemonIndexList/pokemonIndexList.js";
+import PokemonList from "../pokemonList/pokemonList.js";
 import Loading from "../loading/loading.js";
 import InfoJumbotron from "../infoJumbotron/jumbotron.js";
+import Buttons from "../buttons/buttons.js";
+import ErrorMessage from "../errorMessage/errorMessage.js";
 
 export default function HomePage() {
   const [offset, setOffset] = useState(0);
   const { data, error, loading } = useFetch(getPage, offset);
+
+  if (data) {
+    return (
+      <>
+        <div className="page-buttons">
+          <Buttons
+            disabled={offset === 0 ? true : false}
+            onClick={() => {
+              setOffset(offset - 20);
+            }}
+            text="Previous page"
+          />
+          <Buttons
+            disabled={offset > data.count ? true : false}
+            onClick={() => {
+              setOffset(offset + 20);
+            }}
+            text="Next page"
+          />
+        </div>
+        <PokemonList {...data} />
+        <InfoJumbotron {...data} />
+      </>
+    );
+  }
   return (
     <>
       {loading && <Loading />}
-      {data && <PokemonIndexList {...data} />}
-      {error && console.error(error)}
-      <InfoJumbotron {...data} />
+      {error && <ErrorMessage error={error} />}
     </>
   );
 }
