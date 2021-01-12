1154915084bb75715ceb15b90e58b139527a7a2a
juan <juancraft12yt@gmail.com>
2021-01-11 22:10:03 -0300

    Modify loading component

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./loading.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Loading() {
-  return <h1>Loading....</h1>;
+  return (
+    <div className="card loading-card">
+      <div
+        className="spinner-border text-danger loading-circle"
+        role="status"
+      />
+      <h1 className="loading-text">Loading...</h1>
+    </div>
+  );
 }
